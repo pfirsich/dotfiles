9b59d0b2b09326103dba5814bf676096fee674c6
Joel Schumacher <joelschum@gmail.com>
2020-03-06 20:11:11 +0100

    Add local bin and vcpkg root to config.fish

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -1,6 +1,7 @@
-set -gx PATH /sbin /usr/sbin /home/joel/bin /home/joel/.local/bin $PATH
+set -gx PATH /sbin /usr/sbin /home/joel/bin /home/joel/go/bin /home/joel/.local/bin $PATH
 set -gx VISUAL vim
 set -gx EDITOR vim
+set -gx VCPKG_ROOT /home/joel/repos/vcpkg
 
 abbr "-" "prevd"
 abbr "+" "nextd"
