edd758cf19d8910a4e90da2122ac1ca1f1e20008
Joel Schumacher <joelschum@gmail.com>
2022-09-25 16:22:23 +0200

    Add luarocks and GOPATH to PATH

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -1,4 +1,5 @@
-set -gx PATH /sbin /usr/sbin /home/joel/bin /home/joel/go/bin /home/joel/.local/bin $PATH
+set -gx GOPATH /home/joel/.go
+set -gx PATH /sbin /usr/sbin /home/joel/bin $GOPATH/bin /home/joel/.local/bin /home/joel/.luarocks/bin $PATH
 set -gx VISUAL vim
 set -gx EDITOR vim
 set -gx VCPKG_ROOT /home/joel/repos/vcpkg
