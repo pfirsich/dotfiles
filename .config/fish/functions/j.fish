c2560c128e273f631ad37fdc40b77a234770b4df
Joel Schumacher <joelschum@gmail.com>
2022-09-25 16:23:07 +0200

    Read j paths from ~/.jpaths

--- .config/fish/functions/j.fish
+++ .config/fish/functions/j.fish
@@ -1,21 +1,10 @@
 function j --description "j for jump"
-    switch "$argv"
-        case 'dev'
-            cd ~/dev
-        case 'ff'
-            cd ~/.config/fish/functions
-        case 'dot'
-            cd ~/repos/dotfiles
-        case 'dl'
-            cd ~/Downloads
-        case '*'
-            while read -la line
-                set -l name (string match -r "^[^=]+" $line)
-                set -l dir (string match -r "[^=]+\$" $line)
-                if [ $name = $argv ]
-                    cd $dir
-                    break
-                end
-            end < ~/.jpaths
-    end
+    while read -la line
+        set -l name (string match -r "^[^=]+" "$line")
+        set -l dir (string match -r "[^=]+\$" "$line")
+        if [ $name = $argv ]
+            cd "$dir"
+            break
+        end
+    end < ~/.jpaths
 end
